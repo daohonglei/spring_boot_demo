7e45bdbbeda1d8738edb5cecf73d5f84de064094
daohonglei <858883986@qq.com>
2018-03-20 16:56:33 +0800

    添加模板

--- demo/src/main/java/com/example/demo/Index.java
+++ demo/src/main/java/com/example/demo/Index.java
@@ -5,13 +5,25 @@
 
 package com.example.demo;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pojo.User;
 
 @RestController
 public class Index {
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	@RequestMapping("/getUser")
+	public User getUser() {
+		User u=new User();
+		u.setAge(18);
+		u.setName("ldh");
+		u.setBirthday(new Date());
+		return u;
 	}
 }
--- demo/src/main/java/com/example/demo/UserAction.java
+++ demo/src/main/java/com/example/demo/UserAction.java
@@ -7,23 +7,63 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.pojo.User;
 
 @Controller
+@RequestMapping("/user")
 public class UserAction {
-	@RequestMapping("/user")
+	@RequestMapping(value="/users",method=RequestMethod.GET)
 	public String user(Model model) {
-		model.addAttribute("name","ldh");
 		User u = new User();
 		model.addAttribute("user", u);
+		return "userlist";
+	}
+	
+	@RequestMapping(value="/add",method=RequestMethod.GET)
+	public String add(Model model) {
+		User u = new User();
+		model.addAttribute("user", u);
+		model.addAttribute("method","/user/add");
 		return "user";
 	}
-	@RequestMapping("/postform")
-	public String postform(User user) {
+	@RequestMapping(value="/add",method=RequestMethod.POST)
+	public String add(User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
-		return "redirect:/user";
+		return "redirect:/user/users";
+	}
+	
+	@RequestMapping(value="/update/{id}",method=RequestMethod.GET)
+	public String update(Model model,@PathVariable("id") int id) {
+		System.out.println(id);
+		User u = new User();
+		u.setAge(18);
+		u.setName("233");
+		model.addAttribute("user", u);
+		model.addAttribute("method","/user/update");
+		return "user";
+	}
+	@RequestMapping(value="/update",method=RequestMethod.POST)
+	public String update(User user) {
+		System.out.println(user.getName());
+		System.out.println(user.getAge());
+		return "redirect:/user/users";
+	}
+	@RequestMapping(value="/delete/{id}",method=RequestMethod.GET)
+	public String delete(@PathVariable("id") int idd) {
+		System.out.println(idd);
+		return "redirect:/user/users";
+	}
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public String test() {
+		return "test";
+	}
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public String home() {
+		return "home";
 	}
 }
